91723dff357910de02d6af7a9e2fd5488d7824bc
wbsachin <sachin.m@webvillee.in>
2023-06-30 15:48:24 +0530

    new changes

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -7,7 +7,11 @@ jenkin_greetings = function(req,res){
     res.json({status:200, success:true, message: 'Welcome to Jenkin Server'});
 };
 
+test = function(req,res){
+    res.json({status:200, success:true, message: 'Welcome to test Server'});
+};
 module.exports = {
     greetings,
-    jenkin_greetings
+    jenkin_greetings,
+    test
 }
--- router/my.router.js
+++ router/my.router.js
@@ -3,5 +3,5 @@ var testcontroller = require('../controller/test.controller');
 
 router.get('/greetings', testcontroller.greetings);
 router.get('/jenkin_greetings', testcontroller.jenkin_greetings);
-
+router.get('/test', testcontroller.test);
 module.exports = router;
